726aee780eeff89b6a92332af1c783781ddac63d
TFNeto <tiago8filipe@gmail.com>
2018-12-30 19:41:48 +0000

    Intrinsic gui update + picture taking/saving(problem with save and no button)

--- ObjectTracking/intrinsic.cpp
+++ ObjectTracking/intrinsic.cpp
@@ -19,15 +19,17 @@ Intrinsic::Intrinsic(QWidget *parent) :
     for (uint i = 0; i<listOfCameras.size();i++ )
     {
         Camera cam = listOfCameras[i];
-        ui->cameraDropdown->addItem(QString::fromStdString(cam.getName()));
+        ui->cameraDropdown->addItem(QString::fromStdString(cam.getIP())+ " ( "+QString::fromStdString(std::to_string(cam.getX()))+" , "+QString::fromStdString(std::to_string(cam.getY()))+" , "+QString::fromStdString(std::to_string(cam.getZ()))+" )");
     }
-    ui->verticalSlider->setVisible(false);
+
     ui->pictureButton->setVisible(false);
     ui->numPicsDropdown->setValue(30);
     ui->label_3->setVisible(false);
     ui->label_4->setVisible(false);
     ui->saveButton->setVisible(false);
     ui->repeatButton->setVisible(false);
+    ui->saveImage->setVisible(false);
+    ui->saveImageButton->setVisible(false);
     //QPixmap pix;
     //ui->label_CameraFeed->setPixmap(pix.scaled(420, 280, Qt::KeepAspectRatio));
 }
@@ -47,27 +49,23 @@ void Intrinsic::on_startCalibrButton_clicked()
 
 void Intrinsic::on_pictureButton_clicked()  // here is where the MAIGC HAPPENS
 {
-    counter = counter + 1;
+   // counter = counter + 1;
     //take picture with open cv, save the picture to %foldername%
     //check if images are corrupt?
-    ui->verticalSlider->setValue(counter);
+    ui->pictureButton->setVisible(false);
 
-    if  (counter == ui->verticalSlider->maximum())
-    {
-        double errorVal = 10.3243;
+    string fileName;
+    FlyCapture2::IPAddress ipa = 2852045226;
 
-        //end of picture-taking phase
-        ui->pictureButton->setVisible(false);
-        ui->verticalSlider->setValue(0);
+    fileName = takeSinglePictureFromSingleCamera(ipa);
+    QImage myImage;
+    myImage.load(QString::fromStdString(fileName),"png");
 
-        //errorVal = intrinsic_compute::run(); //compute intrinsic calibration for a single camera
+    ui->label_CameraFeed->setPixmap(QPixmap::fromImage(myImage).scaled(630,420,Qt::KeepAspectRatio));
+    ui->label_CameraFeed->repaint();
 
-        ui->label_3->setVisible(true);
-        ui->label_4->setVisible(true);
-        ui->label_4->setText(QString::fromStdString(to_string(errorVal)));
-        ui->saveButton->setVisible(true);
-        ui->repeatButton->setVisible(true);
-    }
+    ui->saveImage->setVisible(true);
+    ui->saveImageButton->setVisible(true);
 
 }
 
@@ -76,8 +74,6 @@ void Intrinsic::calibrateCamera()
     // calibrate camera is misleading - actually starts a picture-taking routine for future calibration
 
     /*
-    ui->label_3->setVisible(false);
-    ui->label_4->setVisible(false);
     ui->saveButton->setVisible(false);
     ui->repeatButton->setVisible(false);
     int numOfPic = ui->numPicsDropdown->value();
@@ -88,9 +84,9 @@ void Intrinsic::calibrateCamera()
     //open camera frame with open cv
     ui->verticalSlider->setVisible(true);
     ui->verticalSlider->setMaximum(numOfPic);
-    ui->pictureButton->setVisible(true);
+
     */
-    cout << "AAAAAAAAAAAAAAAAA" << endl;
+
     // Get camera index from the dropdown menu
     //int cameraIndex = ui->cameraDropdown->currentIndex();
     //string ip = listOfCameras[cameraIndex].getIP();
@@ -98,21 +94,8 @@ void Intrinsic::calibrateCamera()
     // Get number of pictures required
     //int numOfPic = ui->numPicsDropdown->value();
     // Show live feed
-    string fileName;
-    cout << "0000000000000" << endl;
-    FlyCapture2::IPAddress ipa = 2852045226;
-    for (int i = 0; i < 10; i++)
-    {
-      //  cout << "11111111111111" << endl;
-        fileName = takeSinglePictureFromSingleCamera(ipa);
-    //    cout << "fileName" << fileName << endl;
-        QImage myImage;
-        myImage.load(QString::fromStdString(fileName),"png");
 
-        ui->label_CameraFeed->setPixmap(QPixmap::fromImage(myImage).scaled(420,280,Qt::KeepAspectRatio));
-        ui->label_CameraFeed->repaint();
-     //   cout << "44444444444444" << endl;
-    }
+
     // string fileName = takeSinglePictureFromSingleCamera(0);
     //QPixmap pix(QString::fromStdString(fileName));
     //ui->label_CameraFeed->setPixmap(pix.scaled(420, 280, Qt::KeepAspectRatio));
@@ -122,6 +105,8 @@ void Intrinsic::calibrateCamera()
         // In each iteration:
             // Show the picture on the labelPic component
             // Increase the slider count by 1
+
+     ui->pictureButton->setVisible(true);
 }
 
 void Intrinsic::on_closeButton_clicked()
@@ -148,3 +133,5 @@ void Intrinsic::on_loadButton_clicked()
 {
     //load parameters from xml file
 }
+
+
