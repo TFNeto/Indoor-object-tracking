207a862969abd1ad16e0b2e228e8331a4c6df120
Diogo Cardoso <tuniq@live.com.pt>
2019-01-08 18:45:42 +0000

    Cleanup code

--- ObjectTracking/configuration.cpp
+++ ObjectTracking/configuration.cpp
@@ -13,10 +13,6 @@
 
 #include "camerafly.h"
 
-//#include "stdafx.h"
-//#include "FlyCapture2.h"
-
-//using namespace FlyCapture2;
 using namespace std;
 
 Configuration::Configuration(QWidget *parent) :
@@ -36,37 +32,6 @@ void Configuration::on_close_pushButton_clicked()
 {
     this->close();
 }
-
-/*void Configuration::on_addCam_push_clicked()
-{
-    ui->tableWidget->setRowCount(ui->tableWidget->rowCount()+1);
-    QTableWidgetItem *item0 = new QTableWidgetItem;
-    item0->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 0, item0);
-    QTableWidgetItem *item1 = new QTableWidgetItem;
-    item1->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 1, item1);
-    QTableWidgetItem *item2 = new QTableWidgetItem;
-    item2->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 2, item2);
-    QTableWidgetItem *item3 = new QTableWidgetItem;
-    item3->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 3, item3);
-    QTableWidgetItem *item4 = new QTableWidgetItem;
-    item4->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 4, item4);
-    QTableWidgetItem *item5 = new QTableWidgetItem;
-    item5->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 5, item5);
-    QTableWidgetItem *item6 = new QTableWidgetItem;
-    item6->setText(tr("Attempt"));
-    item6->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 6, item6);
-    QTableWidgetItem *item7 = new QTableWidgetItem;
-    item7->setText(tr("Remove"));
-    item7->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 7, item7);
-}*/
 
 void Configuration::on_tableWidget_cellClicked(int row, int column)
 {
@@ -116,38 +81,35 @@ void Configuration::updateTable(vector<Camera> newListOfCameras)
 
     for (size_t i = 0; i < newListOfCameras.size(); i++)
     {
-        // Create a camera object, so we can access its methods
-        Camera camObj = newListOfCameras[i];
-
         // Set name
         QTableWidgetItem *name = new QTableWidgetItem;
-        name->setText(QString::fromStdString(camObj.getName()));
+        name->setText(QString::fromStdString(newListOfCameras[i].getName()));
         name->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
         ui->tableWidget->setItem(static_cast<int>(i), 0, name);
         // Set IP Address
         QTableWidgetItem *ip = new QTableWidgetItem;
         ip->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-        ip->setText(QString::fromStdString(camObj.getIP()));
+        ip->setText(QString::fromStdString(newListOfCameras[i].getIP()));
         ui->tableWidget->setItem(static_cast<int>(i), 1, ip);
         // Set position X
         QTableWidgetItem *positionX = new QTableWidgetItem;
         positionX->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-        positionX->setText(QString::fromStdString(to_string(camObj.getX())));
+        positionX->setText(QString::fromStdString(to_string(newListOfCameras[i].getX())));
         ui->tableWidget->setItem(static_cast<int>(i), 2, positionX);
         // Set position Y
         QTableWidgetItem *positionY = new QTableWidgetItem;
         positionY->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-        positionY->setText(QString::fromStdString(to_string(camObj.getY())));
+        positionY->setText(QString::fromStdString(to_string(newListOfCameras[i].getY())));
         ui->tableWidget->setItem(static_cast<int>(i), 3, positionY);
         // Set position Z
         QTableWidgetItem *positionZ = new QTableWidgetItem;
         positionZ->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-        positionZ->setText(QString::fromStdString(to_string(camObj.getZ())));
+        positionZ->setText(QString::fromStdString(to_string(newListOfCameras[i].getZ())));
         ui->tableWidget->setItem(static_cast<int>(i), 4, positionZ);
         // Set angle
         QTableWidgetItem *angle = new QTableWidgetItem;
         angle->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-        angle->setText(QString::fromStdString(to_string(camObj.getAngle())));
+        angle->setText(QString::fromStdString(to_string(newListOfCameras[i].getAngle())));
         ui->tableWidget->setItem(static_cast<int>(i), 5, angle);
         // Connection - attempt
         QTableWidgetItem *connectionAttempt = new QTableWidgetItem;
@@ -168,5 +130,4 @@ void Configuration::on_scan_push_clicked()
     // Note: By doing this, we are deleting/ignoring the previous list cameras
     listOfCameras = scanCameras();
     this->updateTable(listOfCameras);
-
 }
